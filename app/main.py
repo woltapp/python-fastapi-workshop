0d03cd658b5a36b5cbe283812e5b3f4d2f3408a2
Jerry Pussinen <jerry.pussinen@gmail.com>
2022-09-21 12:17:00 +0300

    Add the rest of the live coded stuff

--- app/main.py
+++ app/main.py
@@ -2,10 +2,34 @@ import json
 import pathlib
 from typing import List
 
-from fastapi import FastAPI, Query, Path
+from fastapi import FastAPI, HTTPException, Path, Query
 from pydantic import BaseModel
 
 app = FastAPI(title="Restaurant API")
+
+RESTAURANTS = {}
+
+
+@app.on_event("startup")
+def startup_event():
+    restaurants_file_path = pathlib.Path(__file__).parent / "restaurants.json"
+    with open(restaurants_file_path) as restaurants_file:
+        restaurant_data = json.load(restaurants_file)
+    for restaurant_data in restaurant_data["restaurants"]:
+        lon, lat = restaurant_data["location"]
+        restaurant_id = restaurant_data["id"]
+        restaurant = FullRestaurant(
+            name=restaurant_data["name"],
+            description=restaurant_data["description"],
+            id=restaurant_data["id"],
+            location=Location(
+                city=restaurant_data["city"],
+                coordinates=Coordinates(lat=lat, lon=lon),
+            ),
+            tags=restaurant_data["tags"],
+        )
+        RESTAURANTS[restaurant_id] = restaurant
+
 
 @app.get("/")
 def hello_world():
@@ -26,30 +50,45 @@ def showcase_features(
     return {"Hello": "Jerry", "Got my_parameter": my_parameter}
 
 
-
-class Location(BaseModel):
+class LightLocation(BaseModel):
     city: str
 
 
-class Restaurant(BaseModel):
+class Coordinates(BaseModel):
+    lat: float
+    lon: float
+
+
+class Location(BaseModel):
+    city: str
+    coordinates: Coordinates
+
+
+class LightRestaurant(BaseModel):
+    name: str
+    description: str
+    id: str
+    location: LightLocation
+
+
+class FullRestaurant(BaseModel):
     name: str
     description: str
     id: str
     location: Location
+    tags: List[str]
 
 
-@app.get("/restaurants", response_model=List[Restaurant])
+@app.get("/restaurants", response_model=List[LightRestaurant])
 def get_restaurants():
-    restaurants_file_path = pathlib.Path(__file__).parent / "restaurants.json"
-    with open(restaurants_file_path) as restaurants_file:
-        restaurant_data = json.load(restaurants_file)
-    restaurants = []
-    for restaurant_data in restaurant_data["restaurants"]:
-        restaurant = Restaurant(
-            name=restaurant_data["name"],
-            description=restaurant_data["description"],
-            id=restaurant_data["id"],
-            location=Location(city=restaurant_data["city"]),
+    return list(RESTAURANTS.values())
+
+
+@app.get("/restaurants/{restaurant_id}", response_model=FullRestaurant)
+def get_restaurant(restaurant_id: str):
+    if restaurant_id in RESTAURANTS:
+        return RESTAURANTS[restaurant_id]
+    else:
+        raise HTTPException(
+            status_code=404, detail="Sorry, this restaurant does not exist"
         )
-        restaurants.append(restaurant)
-    return restaurants
